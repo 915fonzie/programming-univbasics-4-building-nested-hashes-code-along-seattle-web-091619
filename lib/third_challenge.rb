8830cccda75753f1c14975edd16e2e315a4da429
Alfonso Pruneda-Suarez <alfonso.pru@outlook.com>
2019-09-08 22:30:09 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -13,10 +13,7 @@ def third_challenge
         #your key/value pairs here
         name: "Romeo", age: "15", status: "alive"
         },
-      :hero_friends => [patriarch: {}
-      matriarch: {}
-      hero: {}
-      hero_friends: []
+      :hero_friends => []
    }, 
    :capulet => {
       :patriarch => {
